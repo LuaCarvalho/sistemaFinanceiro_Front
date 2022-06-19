6e164b0c093843bdf92112d961f2899bf0011e76
LuaCarvalho <luacarvalho2008@hotmail.com>
2022-06-19 18:44:55 -0300

    Progresso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState} from 'react';
+import api from './services/api';
+import './adminlte.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const[conta, setConta] = useState();
+  const [movimento, setMovimento] = useState();
+
+  useEffect( () => {
+    api
+    .get("/conta/2")
+    .then((response) => setConta(response.data))
+    .catch((err)=> {
+    console.error("Erro de conexão com a API" + err);
+   });
+  }, []);
+
+  class Conta {
+    constructor(id, codigobanco, nomebanco, numero, operacao, saldo){ 
+      this.id = id;
+      this.codigobanco = codigobanco;
+      this.nomebanco = nomebanco;
+      this.numero = numero;
+      this.operacao = operacao;
+      this.saldo = saldo;
+  }
+  }
+
+ let contaBradesco = new Conta(8, 321, "Bradesco", 654, 13, 198.5);
+ console.log(contaBradesco);
+ console.log(conta);
+ 
+  return(
+   <div> 
+     <p> ID: {conta?.id} </p>
+     <p>Código do Banco: {conta?.codigobanco}</p>
+     <p>Nome do Banco: {conta?.nomebanco}</p>
+     <p>Agência: {conta?.agenciabanco}</p>
+     <p>Número da Conta: {conta?.numero}</p>
+     <p>Operação: {conta?.operacao}</p>
+     <p>Saldo: {conta?.saldo}</p>
+     <br></br>
+     <p>Descrição do Movimento: {movimento?.descricao} </p>
+     <p>Valor do Movimento: {movimento?.valor} </p>
     </div>
-  );
-}
 
-export default App;
+    
+  );
+
+}
